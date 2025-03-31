314fcc0dcce6b84f61df1d372f6bbe40fe9ed529
yyyoichi <origamiichiro@gmail.com>
2025-03-31 23:01:27 +0000

    feat: add FunOut function

--- funin.go
+++ funin.go
@@ -2,8 +2,33 @@ package piter
 
 import (
 	"context"
+	"runtime"
 	"sync"
 )
+
+func FunOut[I, O any](ctx context.Context, src <-chan I, fn func(I) O) <-chan O {
+	c := runtime.GOMAXPROCS(0)
+	outChs := make([]chan O, c)
+	for i := range c {
+		outCh := make(chan O)
+		go func() {
+			defer close(outCh)
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case in, ok := <-src:
+					if !ok {
+						return
+					}
+					outCh <- fn(in)
+				}
+			}
+		}()
+		outChs[i] = outCh
+	}
+	return FunIn(ctx, outChs...)
+}
 
 func FunIn[T any](cxt context.Context, channels ...chan T) <-chan T {
 	var wg sync.WaitGroup
